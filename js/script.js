370a0ffb52fa38396937a937de2cc03fff678d76
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-05-26 15:27:35 +0200

    add start variable and full price of ticket

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log('JS OK');
+//create variabili per costo biglietto eta` e km  
+const ticketPriceForKm = 0.21;
+
+let km;
+
+let age;
+
+//prompt per chidere dati all'utente
+km = prompt('quanti Kilometri si desidera percorrere?', 100);
+console.log(km);
+
+age = prompt('Quanti anni hai?', 20);
+console.log(age);
+
+//prezzo del biglietto intero
+let ticketTotalPrice = km * ticketPriceForKm;
+console.log(ticketTotalPrice)
